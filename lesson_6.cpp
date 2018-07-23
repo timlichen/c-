3d4744e96feb26064736d3d9929f37f19e6f8d97
Tim Chen <tim.li.chen@gmail.com>
2018-07-23 19:17:06 -0400

    .

---
+++ lesson_6.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+
+    string myString( "myString!" );
+    myString[0] = 'y';
+    cout << myString;
+
+    return 0;
+}
